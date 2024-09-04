2d6b536a6052ec8fde494a79a0cadf98594ddbd4
Mark Rustad <mark.rustad@nih.gov>
2024-09-04 17:57:01 -0400

    modify setup.py to allow for additional scripts
    Troubleshooting this error: Multiple top-level packages discovered in a flat-layout: ['nnunetv2', 'scripts_mdr']. setuptools expects a single package or an explicitly defined package structure.

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-import setuptools
+from setuptools import setup, find_packages
 
-if __name__ == "__main__":
-    setuptools.setup()
+setup(
+    name='nnunet',
+    version='0.1',
+    packages=find_packages(include=['nnunetv2*', 'scripts_mdr*']),  # Explicitly include your packages
+    # Add other necessary metadata
+)
