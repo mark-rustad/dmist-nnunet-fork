18b6f39ec051eeb3641e656fa56e52654eabd739
Mark Rustad <mark.rustad@nih.gov>
2024-10-02 18:14:34 -0400

    rename package in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nnunet',
-    version='0.1',
-    packages=find_packages(include=['nnunetv2*', 'irf_scripts*']),  # Explicitly include your packages
+    name="nnunet",
+    version="0.1",
+    packages=find_packages(include=["nnunetv2*", "dmist_nnunet*"]),  # Explicitly include your packages
     # Add other necessary metadata
 )
