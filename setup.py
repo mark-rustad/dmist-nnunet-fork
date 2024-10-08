b878ce45e205704d5ea88de767f2d1296b2c3b54
Mark Rustad <mark.rustad@nih.gov>
2024-10-08 03:12:50 -0400

    fix name in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="nnunet",
+    name="nnunetv2",
     version="0.1",
-    packages=find_packages(include=["nnunetv2*", "dmist_nnunet*"]),  # Explicitly include your packages
+    # packages=find_packages(include=["nnunetv2*", "dmist_nnunet*"]),  # Explicitly include your packages
+    packages=find_packages(include=["nnunetv2*"]),  # Explicitly include your packages
     # Add other necessary metadata
 )
